56bea10d38abd8af5c067f0cafc9e1c152e996b3
iniyan <viniyan563@gmail.com>
2025-10-26 12:40:57 +0530

    initial setup with javafx

---
+++ src/main/java/com/notia/App.java
@@ -0,0 +1,25 @@
+package com.notia;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Welcome to Notia!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 640, 480);
+        
+        stage.setTitle("Notia");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
